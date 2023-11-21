19dd51ac6a46cbfa6de07a62f3fa782af981e068
AvirukBasak <avirukbasak@gmail.com>
2023-11-22 00:17:20 +0530

    na

--- src/helperfn.rs
+++ src/helperfn.rs
@@ -1,9 +1,8 @@
 use std::path::Path;
 
-pub fn get_filename(filepath: &str) -> String {
+pub fn get_filename(filepath: &str) -> &str {
     Path::new(filepath)
         .file_name()
         .and_then(|s| s.to_str())
         .expect("error getting filename")
-        .to_owned()
 }
