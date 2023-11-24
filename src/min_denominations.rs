0ea217af00a367f1b4a2b677c675d98ec183b65b
AvirukBasak <avirukbasak@gmail.com>
2023-11-24 12:03:44 +0530

    added memoization and refactored

--- src/min_denominations.rs
+++ src/min_denominations.rs
@@ -3,26 +3,27 @@ use super::helperfn::input;
 use memoize::memoize;
 
 #[memoize]
-fn min_denom(dens: &Vec<i32>, sum: i32) -> i32 {
+fn min_denom(dens: *const Vec<i32>, sum: i32) -> i32 {
     if sum == 0 {
         return 0;
     }
     let mut res = std::i32::MAX;
-    for den in dens.iter() {
+    unsafe { for den in (*dens).iter() {
         if den > &sum {
             continue;
         }
-        let ret = res.min(1 + min_denom(dens, sum - den));
+        let ret = min_denom(dens, sum - den);
         if ret != std::i32::MAX {
-            res = ret;
+            res = res.min(1 + min_denom(dens, sum - den));
         }
-    }
+    }}
     return res;
 }
 
 pub fn min_denominations() {
     let sum: i32 = input(&format!("{}: Enter sum: ", get_filename(file!())));
-    let denominations = vec![25, 10, 5];
-    let result = min_denom(&denominations, sum);
+    let denominations = vec![500, 200, 100, 50, 20, 10, 5, 2, 1];
+    // let denominations = vec![25, 10, 5];
+    let result = min_denom(&*&denominations, sum);
     println!("{}: Result = {result}", get_filename(file!()));
 }
