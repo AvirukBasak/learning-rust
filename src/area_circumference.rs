e0909bd4b290ae4f37bbbebc5d8bf1618f7f999e
AvirukBasak <avirukbasak@gmail.com>
2023-12-02 14:54:47 +0530

    na

--- src/area_circumference.rs
+++ src/area_circumference.rs
@@ -2,7 +2,7 @@ use super::helperfn::get_filename;
 
 pub fn area_circumference() {
     let rad = 3.0;
-    const PI: f32 = 3.14;
+    const PI: f32 = std::f32::consts::PI;
     let area = PI * (rad as f32).powf(2.0);
     let circum = 2.0 * PI * rad;
     println!("{}: Area = {}", get_filename(file!()), area);
--- src/cyl_vol.rs
+++ src/cyl_vol.rs
@@ -2,7 +2,7 @@ use super::helperfn::get_filename;
 use super::helperfn::input;
 
 pub fn cyl_vol() {
-    const PI: f32 = 3.14;
+    const PI: f32 = std::f32::consts::PI;
     let rad: f32 = input(&format!(
         "{}: Enter radius of cylinder: ",
         get_filename(file!())
