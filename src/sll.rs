52bb4e174f722de9ee827f3547dda5f3a7e411b9
AvirukBasak <avirukbasak@gmail.com>
2023-12-05 20:53:49 +0530

    i hate rust

--- src/sll.rs
+++ src/sll.rs
@@ -1,73 +1,69 @@
-// Define the node of the linked list
 struct Node<T> {
     data: T,
     next: Option<Box<Node<T>>>,
 }
 
-// Define the linked list
-pub struct LinkedList<T> {
-    head: Option<Box<Node<T>>>,
+impl<T> Node<T> {
+    fn boxed(data: T, next: Option<Box<Node<T>>>) -> Box<Self> {
+        Box::new(Node { data, next })
+    }
 }
 
-impl<T> LinkedList<T> {
-    // Create a new empty linked list
+pub struct LinkedList<'a, T> {
+    front: Option<Box<Node<T>>>,
+    back: Option<&'a mut Box<Node<T>>>,
+    length: i64,
+}
+
+impl<'a, T> LinkedList<'a, T> {
     pub fn new() -> Self {
-        LinkedList { head: None }
+        LinkedList { front: None, back: None, length: 0 }
     }
-
-    // Add a new element to the front of the linked list
-    pub fn push(&mut self, data: T) {
-        let new_node = Box::new(Node {
-            data,
-            next: self.head.take(),
-        });
-
-        self.head = Some(new_node);
+    pub fn length(&self) -> i64 {
+        self.length
     }
-
-    // Insert a new element at the end of the linked list
-    pub fn append(&mut self, data: T) {
-        let new_node = Box::new(Node { data, next: None });
-
-        let mut current = &mut self.head;
-        while let Some(node) = current {
-            if node.next.is_none() {
-                node.next = Some(new_node);
-                return;
-            }
-            current = &mut node.next;
+    pub fn push(&'a mut self, data: T) {
+        let new_node = Node::boxed(data, self.front.take());
+        self.front = Some(new_node);
+        if self.back.is_none() {
+            self.back = self.front.as_mut();
         }
-
-        // If the list is empty, set the new node as the head
-        self.head = Some(new_node);
+        self.length += 1;
     }
-
-    // Pop the element from the front of the linked list, if any
-    pub fn pop(&mut self) -> Option<T> {
-        self.head.take().map(|node| {
-            self.head = node.next;
+    pub fn append(&'a mut self, data: T) {
+        let new_node = Node::boxed(data, None);
+        if let Some(back) = &mut self.back {
+            back.next = Some(new_node);
+            self.back = self.front.as_mut();
+        } else {
+            self.front = Some(new_node);
+            self.back = self.front.as_mut();
+        }
+        self.length += 1;
+    }
+    pub fn pop(&'a mut self) -> Option<T> {
+        self.length -= 1;
+        self.front.take().map(|node| {
+            self.front = node.next;
+            if self.front.is_none() {
+                self.back = None;
+            }
             node.data
         })
     }
-
-    // Get a reference to the head of the linked list
     pub fn peek(&self) -> Option<&T> {
-        self.head.as_ref().map(|node| &node.data)
+        self.front.as_ref().map(|node| &node.data)
     }
 }
 
 pub fn sll() {
     let mut list = LinkedList::new();
-
     list.push(1);
     list.push(2);
     list.push(3);
-
     println!("Peek: {:?}", list.peek()); // Output: Some(3)
-
     list.append(4);
-    println!("Peek: {:?}", list.peek()); // Output: Some(3) - head remains unchanged
-
+    println!("Peek: {:?}", list.peek()); // Output: Some(3) - front remains unchanged
     println!("Pop: {:?}", list.pop());   // Output: Some(3)
     println!("Pop: {:?}", list.pop());   // Output: Some(2)
     println!("Pop: {:?}", list.pop());   // Output: Some(1)
